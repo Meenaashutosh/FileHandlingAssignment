e79710e1d470783cc9881a2f9dc6f2a7d63c2c36
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-08 16:47:40 +0530

    Create Program1.py

--- Program1.py
+++ Program1.py
@@ -1,10 +1,10 @@
-#1.Write a Python program to read an entire text file and print its contents.
+# 1.Write a Python program to read an entire text file and print its contents.
 
 # creating text file 
 file = open("text.txt","wr+") 
 print "file is created",file.name
 # file containg data
-file.write("Hello World,how are you................\nTheir is Award function in zensar") 
+file.write("Hello Zensarian,how are you................\nTheir is Award function in zensar") 
 
 #reading data from file using read line
 file.seek(0,0)
@@ -12,3 +12,10 @@ file.seek(0,0)
 print (file.read())
 file.close()
 
+#output
+'''
+python Program1.py
+file is created text.txt
+Hello Zensarian,how are you................T
+heir is Award function in zensar
+'''

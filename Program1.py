9f09d744d8de72393b87d0e520e021f0c22c6931
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-08 16:37:51 +0530

    Create Program1.py

--- Program1.py
+++ Program1.py
@@ -4,14 +4,11 @@
 file = open("text.txt","wr+") 
 print "file is created",file.name
 # file containg data
-file.write("Hello World") 
-file.write("Their is csr function in zensar") 
-file.write("and all zensarian required to join it... ") 
-file.write("why??? you can enjoy different activity in our organisation......") 
- 
+file.write("Hello World,how are you................") 
+
 #reading data from file using read line
 file.seek(0,0)
-line= file.read()
+
 print (file.read())
 file.close()
 

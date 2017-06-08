9769b864fff02fd339a653d032375cbf3bd22872
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-08 15:36:56 +0530

    Create Program1.py

--- Program1.py
+++ Program1.py
@@ -1,16 +1,16 @@
 #1.Write a Python program to read an entire text file and print its contents.
 
 # creating text file 
-file = open(“text.txt”,”w”) 
-
+file = open("text.txt","w") 
+print "file is created"
 # file containg data
-file.write(“Hello World”) 
-file.write(“Their is csr function in zensar”) 
-file.write(“and all zensarian required to join it... ”) 
-file.write(“why??? you can enjoy different activity in our organisation......”) 
+file.write("Hello World") 
+file.write("Their is csr function in zensar") 
+file.write("and all zensarian required to join it... ") 
+file.write("why??? you can enjoy different activity in our organisation......") 
  
 file.close() 
 
 #reading data from file using read line
-file = open(“testfile.txt”, “r”) 
+file = open("testfile.txt", "r") 
 print file.readlines() 

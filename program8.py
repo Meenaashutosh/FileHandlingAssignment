00525db48821192eb36e087d17b4a726eee8edaa
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-09 16:26:55 +0530

    Create program8.py

---
+++ program8.py
@@ -0,0 +1,11 @@
+# 8.Write a python program to find the longest words
+
+
+def Word():
+    str= "Contentmart is a platform to hire quality tested content writers, we don't charge for services. "
+    long_word=''
+    for str1 in str:
+    if len(l)>len(long):
+        long_word=str1
+    return long
+print word()

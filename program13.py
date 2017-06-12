c08cb5973600244447738c37526dc7458a2b5106
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-12 12:24:39 +0530

    Create program13.py

--- program13.py
+++ program13.py
@@ -4,3 +4,10 @@ with open('ashu1.txt') as f1, open('ashu.txt') as f2:
     for l1, l2 in zip(f1, f2):  
         # l1 from file1.txt, l2 from file.txt 
         print(l1+l2)  
+
+        
+#output
+'''
+[ashutoshmeena111@demo-1 FileHandlingAssignment]$ python program13.py
+How would I Pythonicly scale these values so that A[0] = 0 and A[3] = 100 
+'''

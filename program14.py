75caa91e00849541471821d2976f50bd02068ecd
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-12 12:30:04 +0530

    Create program14.py

--- program14.py
+++ program14.py
@@ -3,3 +3,10 @@
 import random
 lines = open("ashu.txt").read().splitlines()
 print random.choice(lines)
+
+
+#output
+'''
+[ashutoshmeena111@demo-1 FileHandlingAssignment]$ python program14.py
+How would I Pythonicly scale these values so that A[0] = 0 and A[3] = 100 
+'''

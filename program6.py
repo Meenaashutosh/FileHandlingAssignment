5361c1d8061fb61e66a588aac62dade5253ba104
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-12 10:05:42 +0530

    Create program6.py

--- program6.py
+++ program6.py
@@ -4,3 +4,10 @@
 with open ("file.txt", "r") as file:
     var=file.readlines()
     print(var)
+
+    
+#output
+'''
+[ashutoshmeena111@demo-1 FileHandlingAssignment]$ python program6.py
+['redmi\n', 'apple\n', 'appo\n', 'moto\n', 'panasonic\n', 'sony\n', 'vivo\n']
+'''

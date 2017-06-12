dc5ece4d8275368c8310c6af63adf0f090de0f0e
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-12 10:16:07 +0530

    Create program13.py

---
+++ program13.py
@@ -0,0 +1,6 @@
+#13.Write a Python program to copy the contents of a file to another file
+
+with open('file1.txt') as fi1, open('file.txt') as fi2:  
+    for l1, l2 in zip(fi1, fi2):  
+        # l1 from file1.txt, l2 from file.txt 
+        print(l1+l2)  

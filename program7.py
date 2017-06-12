1539ed008d5898d4eb8d7a5d9d71418b6fe492e1
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-12 10:04:13 +0530

    Create program7.py

--- program7.py
+++ program7.py
@@ -5,3 +5,9 @@ with open("file.txt","r") as text:
     for lines in text:
         array.append(lines)
     print(array)
+
+#output
+'''
+[ashutoshmeena111@demo-1 FileHandlingAssignment]$ python program7.py
+['redmi\n', 'apple\n', 'appo\n', 'moto\n', 'panasonic\n', 'sony\n', 'vivo\n']
+'''
